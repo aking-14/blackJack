ece7f9d5125a3d3d0d7e362d95315f35ffa8d00c
aking-14 <aking14@memphis.edu>
2022-06-10 00:29:29 -0500

    Added deck of cards code

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,6 @@
-print("Hello World")
+import random
+
+deckOfCards =  [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14]
+
+random.shuffle(deckOfCards)
+print(deckOfCards)
